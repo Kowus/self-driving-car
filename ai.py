285588b9d2ad3779f412f5087b321d88ed2de951
Kowus <barnabas@elite-education.org>
2018-03-28 16:43:43 +0000

    create architecture for neural network

--- ai.py
+++ ai.py
@@ -6,6 +6,7 @@ Created on Tue Jan 23 10:56:33 2018
 @author: barnabasnomo
 """
 
+
 import numpy as np
 import random
 import os
@@ -15,3 +16,19 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch.autograd as autograd
 from torch.autograd import Variable
+
+
+# Neural Network architecture
+class Network(nn.Module):
+
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        q_values = self.fc2(x)
+        return q_values
