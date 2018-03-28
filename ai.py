ad8f2a72a812f326a5728b1c003d2e2265d4e3e0
Kowus <barnabas@elite-education.org>
2018-03-28 16:38:47 +0000

    start Build Deep Q Network

---
+++ ai.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 23 10:56:33 2018
+
+@author: barnabasnomo
+"""
+
+import numpy as np
+import random
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
